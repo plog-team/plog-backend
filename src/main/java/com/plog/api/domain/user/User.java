32a3821a0c690012c28beb51b00629acb9548f04
조연준 <whduswns0550@naver.com>
2026-06-09 23:31:57 +0900

    Temporarily remove incomplete authentication

--- src/main/java/com/plog/api/domain/user/User.java
+++ src/main/java/com/plog/api/domain/user/User.java
@@ -29,11 +29,8 @@ public class User extends BaseTimeEntity {
     @Column(length = 100)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String password;
-
-    @Column(nullable = false)
-    private boolean emailVerified = false;
 
     @Builder
     private User(String name, String email, String password) {
